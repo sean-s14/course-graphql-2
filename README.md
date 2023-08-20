d7c9ef78c357cf33f5a1ad9213c50ec798c0b29d
Sean Stocker <sean.stocker14@gmail.com>
2023-08-20 21:31:55 +0100

    feat: setup development server with Apollo

---
+++ README.md
@@ -0,0 +1,15 @@
+# GraphQL Course #2
+
+## Setup
+
+Install dependencies
+
+```
+npm i
+```
+
+Run developmnet server
+
+```
+npm run dev
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    cars: [Car!]!
+  }
+
+  type Car {
+    id: ID!
+    color: String!
+    make: String!
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      cars: () => [{ id: 1, color: "blue", make: "Toyota" }],
+    },
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
